50223e37882722ed406d3cdf141d45a494248246
Anand Mohan <andmhn@outlook.com>
2026-01-03 19:43:05 +0530

    Use raw terminal mode with alternate buffer

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 void watch_init(const char *file);
 void watch_once();
 
+void clr_scr();
+void terminal_setup();
+void terminal_restore();
+
 void run(char *cmds[]) {
     pid_t pid = fork();
     if (pid == 0) {
@@ -19,19 +23,35 @@ void run(char *cmds[]) {
 }
 
 void watch_and_run(const char *dir, char *cmds[]) {
-    printf("watching in : %s\n", dir);
     watch_init(dir);
+
+    terminal_setup();
+    clr_scr();
+    printf("watching in : %s\n", dir);
 
     while (1) {
         watch_once();
+
+        clr_scr();
+        printf("watching in : %s\n", dir);
         printf("running: %s\n\n", cmds[0]);
+
         run(cmds);
+
+        fflush(stdout);
     }
 }
 
 void print_help() {
     puts("USAGE:\twatcher <dir> <cmd...>\n\n");
     puts("\tWatch for file changes in directory and run comand");
+}
+
+void handle_exit(int sig) {
+    (void)sig;
+    terminal_restore();
+    fflush(stdout);
+    _exit(-1);
 }
 
 int main(int argc, char *argv[]) {
@@ -42,6 +62,9 @@ int main(int argc, char *argv[]) {
     }
 
     char *dir = argv[1];
+
+    signal(SIGINT, handle_exit);
+    signal(SIGTERM, handle_exit);
 
     watch_and_run(dir, &argv[2]);
 }
---
+++ terminal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void clr_scr() { printf("\033[2J\033[H"); }
+
+void terminal_restore() {
+    // Leaves Alternate Buffer and shows cursor
+    printf("\033[?1049l\033[?25h");
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void terminal_setup() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(terminal_restore);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+    // Enters alternate buffer and hides cursor
+    printf("\033[?1049h\033[?25l\033[H");
+    fflush(stdout);
+}
